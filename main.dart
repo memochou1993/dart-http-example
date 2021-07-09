7550428764d245a7e30101226e0690b5610239cc
Memo Chou <memochou1993@gmail.com>
2021-07-09 15:38:36 +0800

    Update main function

--- main.dart
+++ main.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 void main() async {
   var client = http.Client();
   try {
-    var url = Uri.parse('https://json.epoch.tw/api/records/{code}');
+    var url = Uri.parse('http://example.org');
     print((await client.get(url)).body);
   } finally {
     client.close();
